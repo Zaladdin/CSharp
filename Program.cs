e4c9a4c2ab7cc4263060515531546ac97729fd7c
Zaladdin <31769884+Zaladdin@users.noreply.github.com>
2022-06-02 05:14:37 +0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+using System;
+
+namespace CSharp05Group
+
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string test;
+            int x;
+            
+            Console.WriteLine("Введите текст, которую хотите повторить:");
+            test = Console.ReadLine();
+
+
+            Console.WriteLine("Введите колличество повтора: ");
+            x = Convert.ToInt32(Console.ReadLine());
+
+           
+
+            for (int i=0; i<x; i++)
+            {
+                Console.WriteLine(test);  
+            }
+        }
+    }v
+}
