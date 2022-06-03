fe65977d69e861e787457633ca29e2ba7bcdd22c
Zaladdin <31769884+Zaladdin@users.noreply.github.com>
2022-06-04 00:50:01 +0400

    Add files via upload

---
+++ Programs/Program2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+
+namespace Vurma
+{
+	public static class Program
+	{
+		public static void Main(string[] args)
+		{
+
+           int a;
+           int i;
+          
+           
+           a = Convert.ToInt32(Console.ReadLine());
+           
+           
+           for (i=0; i<10; i++)
+           { 
+           	
+           
+        
+           	Console.WriteLine (a*i);}
+
+
+
+		}
+	}
+}
