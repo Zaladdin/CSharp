7af0d32373b7e74a890a1e428350767609e1feca
Zaladdin <31769884+Zaladdin@users.noreply.github.com>
2022-06-04 14:19:20 +0400

    Add files via upload

---
+++ Programs/Program3/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace HelloWold
+{
+    public static class Program 
+    {         
+        public static void Main()
+        {
+            int a;
+            int sum;
+            sum =0;
+           
+          a = Convert.ToInt32(Console.ReadLine()) ;
+            for( int i=1; i<=a; i++){
+            	
+            	sum += i*i;
+            	
+            	Console.WriteLine(sum);
+            }
+        }
+    }
+}
