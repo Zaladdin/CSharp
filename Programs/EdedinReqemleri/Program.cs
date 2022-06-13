2f00fce221cb73121c0fc1bcb8affe7bec098685
Zaladdin <31769884+Zaladdin@users.noreply.github.com>
2022-06-13 11:49:44 +0400

    Add files via upload

---
+++ Programs/EdedinReqemleri/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Linq;
+
+namespace Ededinreqemleri
+{
+    public static class Program
+    {
+        public static void Main()
+        {
+            int[] arr={1,2,3,6,7,8,23,78,34,12};
+            int val;
+            
+
+
+
+			
+
+			for (int i=0; i<arr.Length; i++)
+			{
+				if(arr[i]<9)
+				{
+					val=arr[i]%10;
+			 	    Console.WriteLine(val);
+				}
+				
+				else
+				{
+					val=arr[i]/10;
+					
+					Console.WriteLine(val);
+					val=arr[i]%10;
+					Console.WriteLine(val);
+				}
+			
+				
+				
+			}
+			
+        }
+    }
+}
