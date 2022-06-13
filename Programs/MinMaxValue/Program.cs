21e38844e6c7fb7c4744759ac3fe8ead51042733
Zaladdin <31769884+Zaladdin@users.noreply.github.com>
2022-06-13 12:49:13 +0400

    Add files via upload

---
+++ Programs/MinMaxValue/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Linq;
+
+namespace MinMaxValue
+{
+	public static class Program
+	{
+		public static void Main()
+		{
+			int[] arr={1,2,3,6,7,8,23,78,34,12};
+		
+			
+			int   max = int.MinValue;
+			int   min = int.MaxValue;
+			
+			for (int i=0; i<arr.Length; i++)
+			{
+				if (arr[i]>max)
+				{
+					max=arr[i];
+				}
+				
+				if (arr[i]<min)
+				{
+					min=arr[i];
+				}
+			}
+			
+			Console.WriteLine(max);
+			Console.WriteLine(min);
+		}
+	}
+}
