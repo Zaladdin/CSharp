74715dfe30918d3035534440e3c0c73e144e6d67
Zaladdin <31769884+Zaladdin@users.noreply.github.com>
2022-06-13 11:59:06 +0400

    Add files via upload

---
+++ Programs/EdedOlan/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+
+namespace Ededolan
+{
+	public static class Program
+	{
+		public static void Main()
+		{
+			int[] arr={1,2,3,6,7,8,23,78,34,12};
+			
+			for (int i=0; i<arr.Length; i++)
+			{
+				if (arr[i]/10==3 ^ arr[i]%10==3)
+				{
+					Console.WriteLine(arr[i]);
+				}
+				
+			}
+
+
+		}
+	}
+}
