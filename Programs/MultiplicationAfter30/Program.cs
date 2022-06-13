3d3c61b013a35fc72d754a00512db072380f7f55
Zaladdin <31769884+Zaladdin@users.noreply.github.com>
2022-06-13 12:52:10 +0400

    Add files via upload

---
+++ Programs/MultiplicationAfter30/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq;
+
+namespace MultiplicationAfter30
+{
+    public static class Program
+{
+	public static void Main()
+	{
+		Console.WriteLine(MultiplicationAfter30 (1));
+	}
+	
+	public static int MultiplicationAfter30(int a)
+	{
+		int mult=1;
+		a=Convert.ToInt32(Console.ReadLine());
+		for (int i=a+1; i<=a+30;i++)
+		{
+			mult=mult*i;
+		}
+		
+		return mult;
+	}
+}
+}
