497cae2f99ae0661a1fd3ecc60f718c36ad48dfd
Zaladdin <31769884+Zaladdin@users.noreply.github.com>
2022-06-13 12:04:02 +0400

    Add files via upload

---
+++ Programs/MathSqrt/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+
+namespace Math.Sqrt
+{
+	public static class Program
+	{
+		public static void Main()
+		{
+			Console.WriteLine(sqrt(49));
+
+		}
+		
+	
+			
+			public static double sqrt(double a)
+        {
+        	a=Convert.ToDouble(Console.ReadLine());
+            double t;
+            double sqrt = a / 2;
+            do
+            {
+                t = sqrt;
+                sqrt = (t + (a / t)) / 2;
+            } while ((t - sqrt) != 0);
+ 
+            return sqrt;
+        }
+		}
+	}
