63ed905bed5dfb988815653077aa2d6fdd424410
Zaladdin <31769884+Zaladdin@users.noreply.github.com>
2022-06-13 11:47:24 +0400

    Add files via upload

---
+++ Programs/DivideBy3/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+
+namespace Divide3
+{
+	public static class Program
+	{
+		public static void Main(string[] args)
+		{
+		//	a=Convert.ToInt32(Console.ReadLine());
+			Console.WriteLine(DivideBy3 (a));
+		}
+			public static bool DivideBy3(int a)
+			{
+			
+			a=Convert.ToInt32(Console.ReadLine());
+			if (a%3==0)
+			{
+				return true;
+			}
+			
+			else 
+			{
+				return false;
+			}
+			 
+
+		}
+	}
+}
