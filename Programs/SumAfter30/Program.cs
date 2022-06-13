f62db828a412112771b660e203ffb439fbbb7ea6
Zaladdin <31769884+Zaladdin@users.noreply.github.com>
2022-06-13 13:02:57 +0400

    Add files via upload

---
+++ Programs/SumAfter30/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+
+namespace SumAfter30
+{
+	public static class Program
+	{
+		public static void Main()
+		{
+			int b=Convert.ToInt32(Console.ReadLine());
+			Console.WriteLine(SummAfter30(b));
+		}
+		
+		public static int SummAfter30(int a)
+		{
+			int sum=0;
+		//	a=Convert.ToInt32(Console.ReadLine());
+			for (int i=a+1; i<a+31; i++)
+			{
+				sum=sum+i;
+			}
+			
+			return sum;
+		}
+	}
+}
