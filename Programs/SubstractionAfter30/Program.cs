08f3cff7f422afb24d9f4dad7a907a0eae65a387
Zaladdin <31769884+Zaladdin@users.noreply.github.com>
2022-06-13 13:01:58 +0400

    Add files via upload

---
+++ Programs/SubstractionAfter30/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+
+namespace SubstractionAfter30
+{
+	public static class Program
+	{
+		public static void Main()
+		{
+			int b=Convert.ToInt32(Console.ReadLine());
+
+			Console.WriteLine(SubsAf30(b));
+		}
+		
+		public static int SubsAf30(int a)
+		{
+			int sub=0;
+			
+			for (int i=a+1; i<=a+30; i++)
+			{
+				sub=sub-i;
+				
+			}
+
+			return sub;
+			
+			
+		}
+	}
+}
