d864d5bb0a908355db87c1725b64a3bdfe31881a
Zaladdin <31769884+Zaladdin@users.noreply.github.com>
2022-06-13 11:42:31 +0400

    Add files via upload

---
+++ Programs/arrSum/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+
+namespace ArrSum
+{
+	public static class Program
+	{
+		public static void Main()
+		{
+			
+			int sum=0;
+			
+			for (int i=0; i<arr.Length; i++)
+			{
+				sum+=arr[i];
+			}
+			
+			Console.WriteLine(sum);
+		}
+	}
+}
