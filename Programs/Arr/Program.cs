31c07d1db8e3987c3cf6653dc755588aea04fb86
Zaladdin <31769884+Zaladdin@users.noreply.github.com>
2022-06-13 11:37:04 +0400

    Add files via upload

---
+++ Programs/Arr/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+
+namespace Arr
+{
+	public static class Program
+	{
+		public static void Main()
+		{
+			
+			int[] arr={1,2,3,4,5,6};
+			int[] arr1= new int [arr.Length];
+			
+			for (int i=0; i<arr1.Length; i++)
+			{
+			Console.WriteLine(arr1[i]);
+			}
+
+		}
+	}
+}
