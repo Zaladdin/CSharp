140fa243b3b76a4fb135895d075555cc775102a6
Zaladdin <31769884+Zaladdin@users.noreply.github.com>
2022-06-13 12:59:44 +0400

    Add files via upload

---
+++ Programs/Program8/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+
+namespace Pr7
+{
+	public static class Program
+	{
+		public static void Main()
+		{
+            int a = Convert.ToInt32(Console.ReadLine());
+
+            int sum =0;
+            string b = Convert.ToString(a);
+            int c =Convert.ToInt32(b.Length);
+           
+            Console.WriteLine($"{c} reqemli ededin  reqemlerinin cemi");
+
+
+            for (int i = 0; i < c; i++)
+            {
+                
+                    sum = sum + a % 10;
+                    a = a / 10;
+                    
+                }
+                Console.WriteLine(sum);
+                
+		}
+	}
+}
