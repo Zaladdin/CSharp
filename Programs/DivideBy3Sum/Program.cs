adc54bfad16db58179802f5f1680f24477d87e5c
Zaladdin <31769884+Zaladdin@users.noreply.github.com>
2022-06-13 11:48:30 +0400

    Add files via upload

---
+++ Programs/DivideBy3Sum/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Linq;
+
+namespace DivideBy3Sum
+{
+	public static class Program
+	{
+		public static void Main()
+		{
+			int x=Convert.ToInt32(Console.ReadLine());
+			Console.WriteLine(DivideSumBy3(x));
+
+		}
+		
+		public static bool DivideSumBy3(int a)
+		
+		{
+			 int sum=Sum(a);
+			 if (sum%3==0)
+			 {
+			 	return true;
+			 }
+			 else 
+			 {
+			 	return false;
+			 }
+			
+		}
+		
+		public static int Sum(int a)
+		{
+			  int sum =0;
+			 
+
+             string b = Convert.ToString(a);
+
+             int c =Convert.ToInt32(b.Length);
+           
+             for (int i = 0; i < c; i++)
+              
+                {
+                    sum = sum + a % 10;
+                    a = a / 10;
+                    
+                }
+                return sum;
+		}
+	}
+}
