7e9ba2bfd7d6fa3d424c25806fd62f68450feb32
Zaladdin <31769884+Zaladdin@users.noreply.github.com>
2022-06-13 13:23:31 +0400

    Update Program.cs

--- Programs/Program5/Program.cs
+++ Programs/Program5/Program.cs
@@ -1,32 +1,32 @@
-﻿using System;
+
+using System;
 using System.Linq;
 
-namespace Program5
+namespace Pr7
 {
 	public static class Program
 	{
 		public static void Main()
 		{
-		
-         int a;
-         int x;
-         int y;
-         int z;
-         int t;
-         int sum;
-         a=Convert.ToInt32(Console.ReadLine());
-         x=a/1000;
-        
-         Console.WriteLine(x);
-         
-         y=(a-x*1000)/100;
-         Console.WriteLine(y);
-         z=(a-x*1000-y*100)/10;
-         Console.WriteLine(z);
-         t=(a-x*1000-y*100-z*10);
-         Console.WriteLine(t);
-         sum=x+y+z+t;
-         Console.WriteLine(sum);
+            int a = Convert.ToInt32(Console.ReadLine());
+
+            int sum =0;
+            string b = Convert.ToString(a);
+            int c =Convert.ToInt32(b.Length);
+           
+            Console.WriteLine($"{c} reqemli ededin  reqemlerinin cemi");
+
+
+            for (int i = 0; i < c; i++)
+            {
+                
+                    sum = sum + a % 10;
+                    a = a / 10;
+                    
+                }
+                Console.WriteLine(sum);
+                
+		}
 	}
 }
-}
+
