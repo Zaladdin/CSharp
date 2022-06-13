d1e951a773757476127cdc2b2054e5bbf2893cf8
Zaladdin <31769884+Zaladdin@users.noreply.github.com>
2022-06-13 12:53:06 +0400

    Add files via upload

---
+++ Programs/PowBy3/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq;
+
+namespace PowBy3
+{
+	public static class Program
+	{
+		public static void Main(string[] args)
+		{
+			//int b=Convert.ToInt32(Console.ReadLine());
+			Console.WriteLine(PowBy3 (3));
+		}
+		
+		public static int PowBy3(int a)
+		{
+			 a=Convert.ToInt32(Console.ReadLine());
+			int pow3= Convert.ToInt32(Math.Pow(a, 3));
+			return pow3;
+		}
+
+		
+		
+
+}
+}
