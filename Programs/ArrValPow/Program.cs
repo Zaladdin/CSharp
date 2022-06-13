018078693b9c5d9396a5528bfd1839fc97c41ab0
Zaladdin <31769884+Zaladdin@users.noreply.github.com>
2022-06-13 11:43:28 +0400

    Add files via upload

---
+++ Programs/ArrValPow/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+
+namespace ArrValPow
+{
+	public static class Program
+	{
+		public static void Main()
+		{
+			int[] arr = { 1, 2, 3, 6, 7, 8, 23, 78, 34, 12 };
+
+			for (int i=0; i<arr.Length; i++)
+			{
+				arr[i]= arr[i]*arr[i];
+				Console.WriteLine(arr[i]);
+			}
+
+		}
+	}
+}
