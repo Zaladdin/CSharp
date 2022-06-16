62dc3615987e0919c4bf6a5dfe18b8cd0848d4df
Zaladdin <31769884+Zaladdin@users.noreply.github.com>
2022-06-16 20:25:33 +0400

    Add files via upload

---
+++ Programs/FindValInAr/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Linq;
+
+namespace FindValinArr
+{
+	public static class Program
+	{
+		public static void Main()
+		{
+			int[] ar1={1, 2, 3, 4, 6, 8, 98};
+			int a= Convert.ToInt32(Console.ReadLine());
+			ValInArr(a,ar1);
+		}
+		
+		public static void ValInArr(int x, int[] arr)
+		{
+			
+
+			
+			for (int i=0; i<arr.Length; i++)
+			{
+				if(x==arr[i])
+				
+				   {
+				   Console.WriteLine("var " + i + "sirada yerlesen element");
+				   	
+				   }
+				else
+				{
+					//Console.WriteLine("yox");
+				}
+				   
+				   
+				 
+				   
+				   	
+		    }
+		    
+		    
+		    
+		}
+		
+	
+	}
+}
