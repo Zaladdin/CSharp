fe3a0d5c86acda26331e4be95670c91c21ad7a26
Zaladdin <31769884+Zaladdin@users.noreply.github.com>
2022-06-17 00:51:03 +0400

    Update Program.cs

--- Programs/SumMaxVal/Program.cs
+++ Programs/SumMaxVal/Program.cs
@@ -10,42 +10,7 @@ namespace SumMaxVal
 		
            int[] a={1,2,3,6,7,8,23,78,34,12};
           
-           //int sum=0;
-           /*a=Convert.ToInt32(Console.ReadLine());
            
-           for (int i=0; i<a.Length; i++){
-           sum = sum + a % 10;
-           a = a / 10;
-           }
-           Console.WriteLine(sum);*/
-           
-           //Console.WriteLine(SumOfArraysItems(a));//1
-          
-/*int[] numbers = { -4, -3, -2, -1, 0, 1, 2, 3, 4 };
-int result = 0;
-foreach(int number in numbers)
-{
-    if(number > 0)
-    {
-        result++;
-    }
-}
-Console.WriteLine($"Число элементов больше нуля: {result}");*/
-/*int[] numbers = { -4, -3, -2, -1,0, 1, 2, 3, 4 };
-             
-int n = numbers.Length; // длина массива
-int k = n / 2;          // середина массива
-int temp;               // вспомогательный элемент для обмена значениями
-for(int i=0; i < k; i++)
-{
-    temp = numbers[i];
-    numbers[i] = numbers[n - i - 1];
-    numbers[n - i - 1] = temp;
-}
-foreach(int i in numbers)
-{
-    Console.Write($"{i} \t");
-}*/
             int temp;
 for (int i = 0; i < a.Length - 1; i++)
 {
