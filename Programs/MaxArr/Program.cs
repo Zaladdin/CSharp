8c9e379c1bceede385637573079a1876a5c4e05b
Zaladdin <31769884+Zaladdin@users.noreply.github.com>
2022-06-16 21:06:29 +0400

    Update Program.cs

--- Programs/MaxArr/Program.cs
+++ Programs/MaxArr/Program.cs
@@ -1,31 +1,17 @@
-﻿using System;
+using System;
 using System.Linq;
 
 namespace MaxArr
 {
-	public static class Program
-	{
-		public static void Main()
-		{
-          int[] array = new int[] { 1,2,3,6,7,8,23,78,34,12 };
- 
-        /*  int max = int.MinValue;
-          for (int i = 0; i < array.Length; i++)
-            {
-              if (array[i] > max)
-             {
-              
-               max = array[i];
-             }
-    
-            }
-               Console.WriteLine(max);*/
-                 
-                 
-                 int maxValue = array.Max();
- int maxIndex = array.ToList().IndexOf(maxValue);
- Console.WriteLine(maxValue);
- Console.WriteLine(maxIndex);
-		}
-	}
+    public static class Program
+    {
+        public static void Main()
+        {
+            int[] array = new int[] { 1, 2, 3, 6, 7, 8, 23, 78, 34, 12 };       
+            int maxValue = array.Max();
+            int maxIndex = array.ToList().IndexOf(maxValue);
+            Console.WriteLine(maxValue);
+            Console.WriteLine(maxIndex);
+        }
+    }
 }
