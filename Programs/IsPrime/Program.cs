82355c6f0ebd8bf9d3dc4e15f61110a4fb98e1bb
Zaladdin <31769884+Zaladdin@users.noreply.github.com>
2022-06-24 17:17:19 +0400

    Add files via upload

---
+++ Programs/IsPrime/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Linq;
+
+namespace IsPrime
+{
+	public static class Program
+	{
+		public static void Main()
+		{
+			int b =0;
+			b=Convert.ToInt32(Console.ReadLine());
+			Console.WriteLine(IsPrime(b));
+
+		}
+		
+		public static bool IsPrime(int a)
+		{
+			for (int i=2; i<Math.Sqrt(a)+1; i++)
+			{
+				if(a%i==0)
+				{
+					return false;
+				}
+				
+			}
+			
+			return true;
+			
+			
+			
+			
+		}
+	}
+}
