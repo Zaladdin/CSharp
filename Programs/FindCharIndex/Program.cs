cfd712946598a6baf362cb8f7ba8253eee457f63
Zaladdin <31769884+Zaladdin@users.noreply.github.com>
2022-06-24 15:16:34 +0400

    Add files via upload

---
+++ Programs/FindCharIndex/Program.cs
@@ -0,0 +1,38 @@
+﻿using System; 
+using System.Linq; 
+  
+ namespace FindCharIndex 
+ { 
+ 	public static class Program 
+ 	{ 
+ 		public static void Main() 
+ 		{
+ 			string word= Console.ReadLine();
+ 			char a=Convert.ToChar(Console.ReadLine());
+ 			
+ 		    FindCharIndex(word,a);
+ 		}
+ 		
+ 		public static void FindCharIndex(string word, char a)
+ 		{
+ 			int x =0;
+ 			for (var i=0;i<word.Length;i++)
+ 			{
+ 				if (word[i]==a)
+ 				{
+ 					x=1;
+ 					Console.WriteLine(i+1 + " herf");
+ 				}
+ 				
+ 				
+ 			}
+ 			if(x==0)
+ 			{
+ 				Console.WriteLine("yoxdur");
+ 			}
+ 			
+ 			
+ 			
+ 		}
+ 	}
+ }
