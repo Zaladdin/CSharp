ff6a8cb1da52a4185e0af7d0898b65f54959a92e
Zaladdin <31769884+Zaladdin@users.noreply.github.com>
2022-06-24 20:20:29 +0400

    Add files via upload

---
+++ Programs/CalcAvg/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Linq;
+
+namespace CalcAvg
+{
+	public static class Program
+	{
+		public static void Main()
+		{
+				int b=0;
+
+
+
+			Console.WriteLine("Fennin sayini yazin");
+
+			b= Convert.ToInt32(Console.ReadLine());
+		int[] arr1=new int [b];
+		
+		for(int i=0;i<arr1.Length; i++)
+		{
+			Console.WriteLine(i+1 + " fennin qiymeti");
+			arr1[i]=Convert.ToInt32(Console.ReadLine());
+		
+			
+			
+		}
+			Console.WriteLine("qiymetlerin ortalamasi");
+			
+			Console.WriteLine(CalcAvg(arr1));
+		
+		
+	
+		
+		}
+		
+		public static int CalcAvg (params int[] arr)
+		{
+		
+			int sum=0;
+			int res=0;
+			foreach(int item in arr)
+			{
+				sum+=item;
+				
+			}
+			res = sum/arr.Length;
+		
+			return res;
+			Console.WriteLine(res);
+			
+		}
+	}
+}
